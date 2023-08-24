d2867cb9759d4a3d4e3d0701995979e38e700b03
Pierre <pgadea@users.noreply.github.com>
2023-08-23 22:51:07 -0500

    final challenge refactor

--- src/app/favorite-card/favorite-card.component.ts
+++ src/app/favorite-card/favorite-card.component.ts
@@ -1,16 +1,18 @@
-import {Component, inject, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TvShowDetails} from '../types';
 import {RouterLink} from '@angular/router';
 import { CountdownPipe } from '../countdown.pipe';
 import { ToggleFavoriteDirective } from '../toggle-favorite.directive';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-favorite-card',
   standalone: true,
-  imports: [CommonModule, RouterLink, ToggleFavoriteDirective, CountdownPipe],
+  imports: [CommonModule, RouterLink, ToggleFavoriteDirective, CountdownPipe, CardComponent],
   templateUrl: './favorite-card.component.html',
-  styleUrls: ['./favorite-card.component.css']
+  styleUrls: ['./favorite-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FavoriteCardComponent {
 
